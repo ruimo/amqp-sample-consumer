c9ff8614b249a4d6fcc9ef114c2a2a202539c2c2
Shisei Hanai <ruimo.uno@gmail.com>
2022-10-31 15:46:52 +0900

    Sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use futures_lite::stream::StreamExt;
+use lapin::{Result, Connection, ConnectionProperties, options::{BasicConsumeOptions, BasicAckOptions}, types::FieldTable};
+use model::User;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value="amqp://127.0.0.1:5672/%2f")]
+    ampq_addr: String,
+    #[arg(short, long, default_value="myqueue")]
+    queue_name: String
+}
+
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    async_global_executor::block_on(async {
+        let conn = Connection::connect(
+            &args.ampq_addr,
+            ConnectionProperties::default(),
+        ).await?;
+
+        let channel = conn.create_channel().await?;
+        
+        let mut consumer = channel.basic_consume(
+            &args.queue_name,
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        ).await?;
+
+        async_global_executor::spawn(async move {
+            while let Some(delivery) = consumer.next().await {
+                let delivery = delivery.expect("error in consumer");
+                let payload = User::deserialize(&delivery.data);
+                println!("Received {:?}", payload);
+                delivery
+                    .ack(BasicAckOptions::default())
+                    .await
+                    .expect("sending ack failed");
+            }
+        }).await;
+
+        Ok(())
+    })
 }
